838c8b4cd75dc19509f9e1cb76bc009344f7472a
Nipun chopra <91chopranipunfree@gmail.com>
2024-01-25 13:03:34 +0530

    make dir structure and push utilities into them.

---
+++ tests/Coderunner/__init__.py
@@ -0,0 +1,4 @@
+from IPaddr.myipaddress_lib import MyipaddressLib
+
+print("Hello to the Main Function")
+MyipaddressLib()
---
+++ utils/IPaddr/__init__.py
@@ -0,0 +1,8 @@
+"""
+My Ipaddress Library for Ip address learning.
+
+This contains common functions for ip address printing and
+manipulation.
+"""
+
+print("Inside My Ip adress Library.")
