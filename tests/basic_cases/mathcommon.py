bef18f9790a18a614658cc461527b7c3c19abd46
Nipun chopra <91chopranipunfree@gmail.com>
2024-10-26 17:01:13 +0530

    Imp dir structure changes for python repo

--- tests/basic_cases/mathcommon.py
+++
@@ -1,7 +0,0 @@
-def commonmath():
-    print(2**7)
-    print("Total IPv4 addresses we can have:",2**32)
-    print(2**64)
-    print("Total IPv6 addresses we can have:",2**128)
-
-commonmath()
--- utils/Datatypes/test_dict.py
+++ utils/Datatypes/test_dict.py
@@ -1,3 +1,5 @@
+from collections import OrderedDict
+
 original_dict = {"best": 5, "is": 2, "of": 9}
 
 for key, val in original_dict.items():
@@ -14,3 +16,39 @@ pairs = [('a', 1), ('b', 2), ('c', 3)]
 new_dict2 = {key: value for key,
              value in pairs if value % 2 == 0}
 print(new_dict2)
+
+clientdict = {
+    "client_config" : {
+
+        "multiple_subnets" : [
+            {
+                "prefix": "172.0.0",
+                "start": "1",
+                "end": "10"
+                },
+            {
+                "prefix": "172.0.0",
+                "start": "1",
+                "end": "10"
+                },
+            {
+                "prefix": "172.0.0",
+                "start": "1",
+                "end": "10"
+            }
+        ]
+    }
+}
+
+if len(clientdict["client_config"]["multiple_subnets"]) == 0:
+    print("Length is Zero")
+else:
+    print("lenght of siubnets is: ",len(clientdict["client_config"]["multiple_subnets"]))
+    print("IO Client subnets to be used are: ", clientdict["client_config"]["multiple_subnets"])
+
+subnetlist = clientdict["client_config"]["multiple_subnets"]
+
+for subnets in subnetlist:
+    print(subnets)
+
+
